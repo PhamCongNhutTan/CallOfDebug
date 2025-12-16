8e54d6b94f40a1285409135f2dbe484c9cef2265
Phạm Công Nhựt Tân <nhuttan2607pm@gmail.com>
2025-12-16 12:14:53 +0700

    add Editor

---
+++ SourceGame/FinalProject/Assets/Editor/Custom.cs
@@ -0,0 +1,39 @@
+
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Custom : EditorWindow
+{
+    [MenuItem("OpenScene/MainMenu", false, 1)]
+    public static void Menu()
+    {
+        EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+        EditorSceneManager.OpenScene("Assets/Scenes/Main.unity");
+    }
+    [MenuItem("OpenScene/OldCity", false, 2)]
+    public static void OldCity()
+    {
+        EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+        EditorSceneManager.OpenScene("Assets/Scenes/OldCity.unity");
+    }
+    [MenuItem("OpenScene/Indus1", false, 3)]
+    public static void Indus1()
+    {
+        EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+        EditorSceneManager.OpenScene("Assets/Scenes/Industry1.unity");
+    }
+    [MenuItem("OpenScene/Indus2", false, 4)]
+    public static void Indus2()
+    {
+        EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+        EditorSceneManager.OpenScene("Assets/Scenes/Industry2.unity");
+    }
+    [MenuItem("OpenScene/Offline", false, 5)]
+    public static void Offline()
+    {
+        EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+        EditorSceneManager.OpenScene("Assets/Scenes/CampaignOffline.unity");
+    }
+}
